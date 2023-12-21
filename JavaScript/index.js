aab5977b5254e975a3b1c454a09553157a0725ef
ankitapawar0608 <kedarpawar775@gmail.com>
2023-12-21 19:20:05 +0530

    offers page added

---
+++ JavaScript/index.js
@@ -0,0 +1,43 @@
+function CoverImg11(){
+    const mobilecover = document.getElementById("mobilecoverimg1");
+    mobilecover.src = "./Images/homepage-img/IMG_7118_2_f6efd42b-f218-4242-b701-78eea56889b4.webp"
+    
+}
+
+function CoverImg12(){
+    const mobilecover = document.getElementById("mobilecoverimg1");
+    mobilecover.src = "./Images/homepage-img/IMG_7127_20231111-180003.webp"
+}
+
+function CoverImg21(){
+    const mobilecover = document.getElementById("mobilecoverimg2");
+    mobilecover.src = "./Images/homepage-img/004_51916a77-a4b1-47ec-b803-1790d6d5642c.webp"
+    
+}
+
+function CoverImg22(){
+    const mobilecover = document.getElementById("mobilecoverimg2");
+    mobilecover.src = "./Images/homepage-img/003_7f76c03c-2965-4818-87b9-40058f666b8b.webp"
+}
+
+function CoverImg31(){
+    const mobilecover = document.getElementById("mobilecoverimg3");
+    mobilecover.src = "./Images/homepage-img/IMG_3318_b636d7b2-1b66-4754-8503-4f8567f4e3bd.webp"
+    
+}
+
+function CoverImg32(){
+    const mobilecover = document.getElementById("mobilecoverimg3");
+    mobilecover.src = "./Images/homepage-img/IMG_3320.webp"
+}
+
+function CoverImg41(){
+    const mobilecover = document.getElementById("mobilecoverimg4");
+    mobilecover.src = "./Images/homepage-img/IMG_2278.webp"
+    
+}
+
+function CoverImg42(){
+    const mobilecover = document.getElementById("mobilecoverimg4");
+    mobilecover.src = "./Images/homepage-img/IMG_2283.webp"
+}
