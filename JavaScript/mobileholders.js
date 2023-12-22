b29729591ecf1d8d308aaf60a593d71a5cf9531b
rohini510 <rohinizaware07@gmail.com>
2023-12-22 13:57:29 +0530

    js added

--- JavaScript/mobileholders.js
+++ JavaScript/mobileholders.js
@@ -11,6 +11,43 @@ function changeAirpod13(imageName) {
     const mainImage = document.getElementById('airpod-main-img3');
     mainImage.src = imageName
 }
+function changeAirpod14(imageName) {
+    const mainImage = document.getElementById('airpod-main-img4');
+    mainImage.src = imageName
+}
+function changeAirpod15(imageName) {
+    const mainImage = document.getElementById('airpod-main-img5');
+    mainImage.src = imageName
+}
+function changeAirpod16(imageName) {
+    const mainImage = document.getElementById('airpod-main-img6');
+    mainImage.src = imageName
+}
+function changeAirpod17(imageName) {
+    const mainImage = document.getElementById('airpod-main-img7');
+    mainImage.src = imageName
+}
+function changeAirpod18(imageName) {
+    const mainImage = document.getElementById('airpod-main-img8');
+    mainImage.src = imageName
+}
+function changeAirpod19(imageName) {
+    const mainImage = document.getElementById('airpod-main-img9');
+    mainImage.src = imageName
+}
+function changeAirpod20(imageName) {
+    const mainImage = document.getElementById('airpod-main-img10');
+    mainImage.src = imageName
+}
+function changeAirpod21(imageName) {
+    const mainImage = document.getElementById('airpod-main-img11');
+    mainImage.src = imageName
+}
+function changeAirpod22(imageName) {
+    const mainImage = document.getElementById('airpod-main-img12');
+    mainImage.src = imageName
+}
+
 
 function inc() {
     const a = document.getElementById("numchange1")
@@ -68,7 +105,7 @@ function inc13() {
     }
     else {
         a.innerHTML = qnt + 1;
-    }
+   }
 
 }
 
@@ -82,4 +119,10 @@ function dec13() {
         a.innerHTML = qnt - 1;
     }
 
+}
+
+
+
+function addtocard(){
+    alert("Add to card successful")
 }
